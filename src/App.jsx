f93e76062410168274bc596df2cbafb6827f728a
artpopor <artpopor@gmail.com>
2024-07-18 13:10:11 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button';
 
 const liff = window.liff;
 
@@ -12,36 +10,22 @@ class App extends Component {
     this.state = {
       name: '',
       userLineID: '',
-      pictureUrl: '',
-      statusMessage: ''
+      pictureUrl: ''
     };
   }
 
-
-  getProfile = async () => {
-    await liff.init({ liffId: `2005869975-nMZOOGBm` }).catch(err => { throw err });
-
-    let getProfile = await liff.getProfile();
-    this.setState({
-      name: getProfile.displayName,
-      userLineID: getProfile.userId,
-      pictureUrl: getProfile.pictureUrl,
-      statusMessage: getProfile.statusMessage
-    });
-
-  }
-
-  sendMessage() {
-    liff.sendMessages([{
-      type: 'text',
-      text: "Hi LIFF"
-    }]).then(() => {
-      liff.closeWindow();
-    });
-  }
-
-  closeLIFF() {
-    liff.closeWindow();
+  componentDidMount = async() => {
+    await liff.init({ liffId: `2005869975-nMZOOGBm` }).catch(err=>{throw err});
+    if (liff.isLoggedIn()) {
+      let getProfile = await liff.getProfile();
+      this.setState({
+        name: getProfile.displayName,
+        userLineID: getProfile.userId,
+        pictureUrl: getProfile.pictureUrl,
+      });
+    }else{
+      liff.login();
+    }
   }
 
   render() {
@@ -49,49 +33,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="support">
-            <img width="25%" src="https://img.icons8.com/color/420/line-me.png" />
-            <img width="25%" src="https://lh3.googleusercontent.com/illfpW97yh9TtvtmtN-BiNcpomys5gzAj4nw8Je6Ydby814PRquAPcvsP2tAV43Iqe8logzjUnjp7tN5Dvk" />
-          </div>
-          <div className="support">
-            {
-              (this.state.pictureUrl && this.state.pictureUrl != '')
-                ?
-                <img width="25%" src={this.state.pictureUrl} />
-                :
-                null
-            }
-          </div>
-          {
-            (this.state.name && this.state.name != '')
-              ?
-              <p>Name: {this.state.name}</p>
-              :
-              null
-          }
-          {
-            (this.state.userLineID && this.state.userLineID != '')
-              ?
-              <p>LineID: {this.state.userLineID}</p>
-              :
-              null
-          }
-          {
-            (this.state.statusMessage && this.state.statusMessage != '')
-              ?
-              <p>statusMessage: {this.state.statusMessage}</p>
-              :
-              null
-          }
-          <div className="support">
-            <Button variant="contained" onClick={this.getProfile.bind(this)} style={{ marginRight: '20px' }} color="primary">
-              Getdata INFO
-            </Button>
-            <Button variant="contained" onClick={this.sendMessage.bind(this)} style={{ marginRight: '20px' }}>
-              Send Message
-            </Button>
-            <Button variant="contained" onClick={this.closeLIFF.bind(this)} color="secondary">
-              Close LIFF
-            </Button>
+            <p>ชื่อ {this.state.name}</p>
+            <p>Line ID {this.state.userLineID}</p>
+            <img alt='pic' src={this.state.pictureUrl} />
           </div>
         </header>
       </div>

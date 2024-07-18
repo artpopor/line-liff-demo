558bd04c03f0cb7a64e115556faf549ae581d419
artpopor <artpopor@gmail.com>
2024-07-18 13:14:10 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const liff = window.liff;
 
-class App extends Component {
+const App = () => {
+  const [name, setName] = useState('');
+  const [userLineID, setUserLineID] = useState('');
+  const [pictureUrl, setPictureUrl] = useState('');
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      userLineID: '',
-      pictureUrl: ''
+  useEffect(() => {
+    const initializeLiff = async () => {
+      try {
+        await liff.init({ liffId: '' });
+        if (liff.isLoggedIn()) {
+          const profile = await liff.getProfile();
+          setName(profile.displayName);
+          setUserLineID(profile.userId);
+          setPictureUrl(profile.pictureUrl);
+        } else {
+          liff.login();
+        }
+      } catch (err) {
+        console.error(err);
+      }
     };
-  }
 
-  componentDidMount = async() => {
-    await liff.init({ liffId: `2005869975-nMZOOGBm` }).catch(err=>{throw err});
-    if (liff.isLoggedIn()) {
-      let getProfile = await liff.getProfile();
-      this.setState({
-        name: getProfile.displayName,
-        userLineID: getProfile.userId,
-        pictureUrl: getProfile.pictureUrl,
-      });
-    }else{
-      liff.login();
-    }
-  }
+    initializeLiff();
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="support">
-            <p>ชื่อ {this.state.name}</p>
-            <p>Line ID {this.state.userLineID}</p>
-            <img alt='pic' src={this.state.pictureUrl} />
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="support">
+          <p>ชื่อ {name}</p>
+          <p>Line ID {userLineID}</p>
+          <img alt="pic" src={pictureUrl} />
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
